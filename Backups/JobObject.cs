dde4c270ea94fdb136a3f08baa36029841bff4f1
akonst <artem3845@gmail.com>
2021-11-07 17:53:23 +0300

    fix: Equals override

--- Backups/JobObject.cs
+++ Backups/JobObject.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Backups
 {
     public class JobObject
@@ -10,5 +12,24 @@ namespace Backups
 
         public string FilePath { get; }
         public string FileName { get; }
+        public string JobName { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((JobObject)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FilePath, FileName);
+        }
+
+        private bool Equals(JobObject other)
+        {
+            return FilePath == other.FilePath && FileName == other.FileName;
+        }
     }
 }
