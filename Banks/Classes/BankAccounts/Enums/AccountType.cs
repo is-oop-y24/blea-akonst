9e08107331b43b095dc47eae8837560e88bc9a6f
akonst <artem3845@gmail.com>
2021-11-27 14:28:32 +0300

    account types service

---
+++ Banks/Classes/BankAccounts/Enums/AccountType.cs
@@ -0,0 +1,14 @@
+namespace Banks.Classes.BankAccounts.Enums
+{
+    public enum AccountType
+    {
+        /// <summary> Debit account </summary>
+        Debit,
+
+        /// <summary> Credit account </summary>
+        Credit,
+
+        /// <summary> Deposit account </summary>
+        Deposit,
+    }
+}
