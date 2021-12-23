ef45156f45a807ae21e9092e06b3fae29677baed
akonst <artem3845@gmail.com>
2021-12-23 18:19:33 +0300

    feat: restore algorithm is implemented

---
+++ BackupsExtra/Classes/RestoreAlgorithm.cs
@@ -0,0 +1,10 @@
+namespace BackupsExtra.Classes
+{
+    public static class RestoreAlgorithm
+    {
+        // there should be an implementation of the recovery method for a specific filesystem
+        public static void RestoreFiles(string storagePath, string restorePath)
+        {
+        }
+    }
+}
