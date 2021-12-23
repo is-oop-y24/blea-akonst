5dcf078f4b5781fe88118ce6beb3f2c005bdf2ec
akonst <artem3845@gmail.com>
2021-12-23 19:00:22 +0300

    refactor: implemented for real filesystem

--- BackupsExtra/Classes/RestoreAlgorithm.cs
+++ BackupsExtra/Classes/RestoreAlgorithm.cs
@@ -1,10 +1,12 @@
+using System.IO.Compression;
+
 namespace BackupsExtra.Classes
 {
     public static class RestoreAlgorithm
     {
-        // there should be an implementation of the recovery method for a specific filesystem
         public static void RestoreFiles(string storagePath, string restorePath)
         {
+            ZipFile.ExtractToDirectory(storagePath, restorePath);
         }
     }
 }
